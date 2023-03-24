e098c312f292aa87db889ab612ee38a25ce32caa
nat-belyaeva <seonat19@gmail.com>
2023-03-24 14:44:42 +0300

    new tests

--- tests/task1/locators.spec.js
+++ tests/task1/locators.spec.js
@@ -46,62 +46,91 @@ test('locators', async ({ page }) => {
 
   //by another locator
   await page.locator('ul.b-list-topics>li')
-    .filter({has:page.locator('a[href="./viewtopic.php?t=24798149"]')})
+      .filter({has: page.locator('a[href="./viewtopic.php?t=24798149"]')})
+
+  await page.locator('ul.b-main-navigation>li')
+      .filter({
+        has: page.locator(('.b-main-navigation__advert_another'))
+      })
+
+  await page.locator('ul.b-main-navigation>li')
+      .filter({hasText: 'Найдите мастера'})
+
 })
 
-test('User is able to type in the Seaarch field', async ({ page }) => {
-  const getSearchInput = await page.locator('.fast-search__input')
-  const getSearchModal = await page.locator('#fast-search-modal .modal-dialog')
+  test('User is able to type in the Seaarch field', async ({page}) => {
+    const getSearchInput = await page.locator('.fast-search__input')
+    const getSearchModal = await page.locator('#fast-search-modal .modal-dialog')
+
+    await page.goto('https://www.onliner.by/')
+    await getSearchInput.type('mobile phone')
+    await expect(getSearchModal).toHaveCount(1)
+  })
+
+  test('User is able to open authentification window', async ({page}) => {
+    const getVhodBtn = await page.locator('.auth-bar__item--text')
+    const authForm = await page.locator(('#auth-container .auth-form'))
+
+    await page.goto('https://www.onliner.by/')
+    await getVhodBtn.click()
+    await expect(authForm).toBeVisible()
+  })
+
+  test('User is able to register by email', async ({page}) => {
+    const getVhodBtn = await page.locator('.auth-bar__item--text')
+    const authForm = await page.locator(('#auth-container .auth-form'))
+    const registerLink = await page.locator('.auth-form__description [href="https://profile.onliner.by/registration"]')
+    const registerWord = await page.locator('form:has-text("Регистрация")')
+    const email = await page.getByPlaceholder('Ваш e-mail')
+    const passwordForm = await page.locator('.auth-form__field input').nth(1)
+    const rando = Math.random().toString(36).substring(2, 7)
+    const typeEmail = `tech${rando}@agilquest.net`
+    const typePassword = `Qwerty123@`
+    const repeatPassword = await page.locator('//*[@placeholder="Повторите пароль"]')
+    const policyCheckbox = await page.locator('.i-checkbox__faux')
+    const submitBtn = await page.locator('button.auth-button')
+    const confirmWindow = await page.locator('.auth-form__title')
+
+    await page.goto('https://www.onliner.by/')
+    await getVhodBtn.click()
+    await expect(authForm).toBeVisible()
+
+    await registerLink.click()
+    await expect(registerWord).toBeVisible()
+
+    await email.type(typeEmail)
+    await passwordForm.type(typePassword)
+    await expect(passwordForm).toHaveClass(/auth-input_success/)
+
+    await repeatPassword.type(typePassword)
+    await expect(repeatPassword).toHaveClass(/auth-input_success/)
+
+    await policyCheckbox.click()
+    await expect(policyCheckbox).toBeChecked()
+
+    await submitBtn.click()
+    await expect(confirmWindow).toHaveText(/Подтвердите/)
+  })
+  test('USer is able to click LoopeBtn', async ({page}) => {
+    const searchLoopeBtn = await page.locator('ul.b-main-navigation>li')
+        .filter({
+          has: page.locator(('.b-main-navigation__advert.b-main-navigation__advert_another '))
+        })
+
+    await page.goto('https://www.onliner.by/')
+    await searchLoopeBtn.click()
+    await expect(page).toHaveURL('https://s.onliner.by/tasks')
+  })
+
+test.only('USer is able to click LoopeBtn by text', async ({page}) => {
+  const searchLoopeBtn = await page.locator('ul.b-main-navigation>li')
+      .filter({hasText: 'Найдите мастера'})
 
   await page.goto('https://www.onliner.by/')
-  await getSearchInput.type('mobile phone')
-  await expect(getSearchModal).toHaveCount(1)
+  await searchLoopeBtn.click()
+  await expect(page).toHaveURL('https://s.onliner.by/tasks')
 })
 
-test('User is able to open authentification window', async ({ page }) => {
-  const getVhodBtn = await page.locator('.auth-bar__item--text')
-  const authForm = await page.locator(('#auth-container .auth-form'))
-
-  await page.goto('https://www.onliner.by/')
-  await getVhodBtn.click()
-  await expect(authForm).toBeVisible()
-})
-
-test('User is able to register by email', async ({ page }) => {
-  const getVhodBtn = await page.locator('.auth-bar__item--text')
-  const authForm = await page.locator(('#auth-container .auth-form'))
-  const registerLink = await page.locator('.auth-form__description [href="https://profile.onliner.by/registration"]')
-  const registerWord = await page.locator('form:has-text("Регистрация")')
-  const email = await page.getByPlaceholder('Ваш e-mail')
-  const passwordForm = await page.locator('.auth-form__field input').nth(1)
-  const rando = Math.random().toString(36).substring(2, 7)
-  const typeEmail = `tech${rando}@agilquest.net`
-  const typePassword = `Qwerty123@`
-  const repeatPassword = await page.locator('//*[@placeholder="Повторите пароль"]')
-  const policyCheckbox = await page.locator('.i-checkbox__faux')
-  const submitBtn = await page.locator('button.auth-button')
-  const confirmWindow = await page.locator('.auth-form__title')
-
-  await page.goto('https://www.onliner.by/')
-  await getVhodBtn.click()
-  await expect(authForm).toBeVisible()
-
-  await registerLink.click()
-  await expect(registerWord).toBeVisible()
-
-  await email.type(typeEmail)
-  await passwordForm.type(typePassword)
-  await expect(passwordForm).toHaveClass(/auth-input_success/)
-
-  await repeatPassword.type(typePassword)
-  await expect(repeatPassword).toHaveClass(/auth-input_success/)
-
-  await policyCheckbox.click()
-  await expect(policyCheckbox).toBeChecked()
-
-  await submitBtn.click()
-  await expect(confirmWindow).toHaveText(/Подтвердите/)
-})
 
 // ======================================== 2 =========================================================
 //2. Дана таблица (см. Вложение - код таблицы максимально упрощён).
@@ -110,4 +139,4 @@ test('User is able to register by email', async ({ page }) => {
 //                                          //tbody/tr[2]/td[5] - Jul 19, 2018 14:43 PM
 
 
-// table/tbody/tr[contains(.,'SimpleDataset')]/td[count(preceding-sibling::td)+1 = count(ancestor::table/thead/tr/th[.='Updated']/preceding-sibling::th)+1]
+//table/tbody/tr[contains(.,'SimpleDataset')]/td[count(preceding-sibling::td)+1 = count(ancestor::table/thead/tr/th[.='Updated']/preceding-sibling::th)+1]
